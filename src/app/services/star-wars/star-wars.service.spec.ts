b2ad9c5021882c145d581a0f7701a28289cf8e55
Jared Youtsey <github@fivedice.net>
2022-06-29 13:59:18 -0500

    more clean tests

--- src/app/services/star-wars/star-wars.service.spec.ts
+++ src/app/services/star-wars/star-wars.service.spec.ts
@@ -1,5 +1,9 @@
-import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { fakeAsync, flush, TestBed } from '@angular/core/testing';
+import { cold } from 'jasmine-marbles';
+import { take, tap } from 'rxjs';
+import { StarWarsCharacter } from './star-wars-character';
+import { StarWarsFilm } from './star-wars-film';
 import { StarWarsService } from './star-wars.service';
 
 describe('StarWarsService', () => {
@@ -18,7 +22,7 @@ describe('StarWarsService', () => {
 		expect(service).toBeTruthy();
 	});
 
-	it('should call correct url', () => {
+	it('should handle getStarWarsFilm$ success', () => {
 		service.getStarWarsFilm$(1).subscribe({
 			next: film => expect(film).toBeDefined,
 			error: fail,
@@ -26,11 +30,57 @@ describe('StarWarsService', () => {
 		controller.expectOne('https://swapi.dev/api/films/1/').flush('');
 	});
 
-	it('should handle api error', () => {
+	it('should handle getStarWarsFilm$ failure', () => {
 		service.getStarWarsFilm$(1).subscribe({
 			next: fail,
 			error: e => expect(e).toBeDefined(),
 		});
 		controller.expectOne('https://swapi.dev/api/films/1/').error(new ProgressEvent('error'));
+	});
+
+	const expectLoading$ToBe = (loading: boolean) => {
+		service.loading$
+			.pipe(
+				take(1),
+				tap(l => expect(l).toEqual(loading))
+			)
+			.subscribe();
+	};
+
+	it('should cycle loading$ when films load', () => {
+		service.loadFilms();
+		expectLoading$ToBe(true);
+		controller.expectOne('https://swapi.dev/api/films').flush('');
+		expectLoading$ToBe(false);
+	});
+
+	it('should cycle loading$ when characters load', fakeAsync(() => {
+		const filmUrlMatch = 'match';
+		service.loadCharactersInFilm({ url: filmUrlMatch } as StarWarsFilm);
+		expectLoading$ToBe(true);
+		controller.expectOne('https://swapi.dev/api/people').flush({
+			next: null,
+			results: [{ films: [''] }, { films: ['', filmUrlMatch, ''] }],
+		});
+		flush();
+		expectLoading$ToBe(false);
+	}));
+
+	const expectFilms$ToBe = (films: Partial<StarWarsFilm>[]) => {
+		expect(service.films$).toBeObservable(cold('a', { a: films }));
+	};
+
+	it('should emit films$ on loadFilms success', () => {
+		expectFilms$ToBe([]);
+		const expectedFilms = [{ title: 'Star Wars' }, { title: 'Empire' }] as StarWarsFilm[];
+		service.loadFilms();
+		controller.expectOne('https://swapi.dev/api/films').flush({ results: expectedFilms });
+		expectFilms$ToBe(expectedFilms);
+
+		// or (but the marbles seem cleaner to me, and they allow us to test at multiple points easier)
+		service.films$.pipe(take(1)).subscribe({
+			next: films => expect(films).toEqual(expectedFilms),
+			error: fail,
+		});
 	});
 });
